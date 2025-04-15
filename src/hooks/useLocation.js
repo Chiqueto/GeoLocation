a93ee113de8a9e09d028ffaacaf4c3abf28dfcef
chiqueto <lfchiqueto@gmail.com>
2025-04-15 16:36:25 -0300

    first commit

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,19 @@
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+
+export default function useLocation() {
+  const [coords, setCoords] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setError("Permissão negada para acessar a localização.");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+    };
+  }, []);
+}
